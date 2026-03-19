6204e751220f99176f88ee4fec3767c8fc3b3db3
Spencer Williams <spnw@plexwave.org>
2026-03-18 22:02:26 -0400

    Update MPC Bar to v0.5.1

--- Formula/mpc-bar.rb
+++ Formula/mpc-bar.rb
@@ -1,13 +1,13 @@
 class MpcBar < Formula
   desc "Menu bar client for the Music Player Daemon"
   homepage "https://github.com/spnw/mpc-bar"
-  url "https://github.com/spnw/mpc-bar/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "659896dbf95d8911c7ebe037dff3ff40e0734938a38d18228558cabd1f56bd59"
+  url "https://github.com/spnw/mpc-bar/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "f8ffd9d04558bc5e85b48829ea7866d10e295bbb8f2529a387411978d695d57d"
   license "GPL-2.0-or-later"
   head "https://github.com/spnw/mpc-bar.git", branch: "main"
 
   depends_on "libmpdclient"
-  depends_on "lua"
+  depends_on "lua@5.4"
 
   def install
     system "make", "BINDIR=#{bin}", "install"
